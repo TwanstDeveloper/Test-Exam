8f755fb1b4242004d92295433b40faae6efbbf13
Twanst Codes <98418359+TwanstDeveloper@users.noreply.github.com>
2023-01-14 15:15:51 +0300

    Calculation simple

---
+++ main.js
@@ -0,0 +1,24 @@
+function jaran() {
+    const x = document.getElementById("num1").value;
+    const y = document.getElementById("num2").value;
+    document.getElementById("kkk").innerHTML = x * y;
+}
+
+function dabash() {
+    const m = document.getElementById("num1").value;
+    const d = document.getElementById("num2").value;
+    document.getElementById("kkk").innerHTML = m / d;
+}
+
+function kam() {
+    const j = document.getElementById("num1").value;
+    const k = document.getElementById("num2").value;
+    document.getElementById("kkk").innerHTML = j - k;
+}
+
+function ko() {
+    var h = document.getElementById("num1").value;
+    var a = document.getElementById("num2").value;
+    var c = parseInt(a) + parseInt(h);
+    document.getElementById("kkk").innerHTML = c;
+}
